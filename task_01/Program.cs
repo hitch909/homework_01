8fc6a016437419480ec3247de5d7833fc50d3973
hitch909 <evgenij909@yandex.ru>
2023-01-14 12:42:07 +0500

    готово

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает два числа и выдает, какое из чисел большее а какое меньшее.
+
+Console.Write("введите первое число");
+int num1 =int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1 > num2)
+{
+    Console.WriteLine($"число {num1} большее из двух , число {num2} меньшее");
+}
+{    
+if (num2 > num1)
+    Console.WriteLine($"число {num2} большее из двух , число {num1} меньшее");
+}
+// готово )).
