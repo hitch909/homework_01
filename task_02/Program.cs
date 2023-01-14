6af79f697684ce9b7cb0f9f45f6f089b9c55e9a5
hitch909 <evgenij909@yandex.ru>
2023-01-14 14:02:34 +0500

    большее из трех готово ))

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -1,2 +1,16 @@
 ﻿// максимальное из трех чисел.
 
+Console.WriteLine("введите три числа");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int max = A;
+if (B>A && B>C)
+{
+   max = B;
+}
+if (C>A && C>B)
+{
+   max = C;
+}
+Console.Write($" {max} большее из трех");
