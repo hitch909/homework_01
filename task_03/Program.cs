92164f595588c67edc2ab87a31a75b5d26ba39b4
hitch909 <evgenij909@yandex.ru>
2023-01-14 14:49:58 +0500

    чет-нечет, Готово ))

--- task_03/Program.cs
+++ task_03/Program.cs
@@ -1,2 +1,12 @@
 ﻿// четное или не четное.
 
+Console.WriteLine("введите любое целое число");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2 == 0)
+{
+    Console.Write($"введенное число {number} является ЧЕТНЫМ");
+}
+if (number % 2 != 0)
+{
+    Console.Write($"введенное число {number} является НЕЧЕТНЫМ");    
+}
