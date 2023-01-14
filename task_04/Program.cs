3291c46195f46f7dd3a0354ee84ccba497a4a7d3
hitch909 <evgenij909@yandex.ru>
2023-01-14 16:47:42 +0500

    четные числа 1-N, готово )))

--- task_04/Program.cs
+++ task_04/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Четные числа в промежутке от 1 до N.
 
+Console.Write("введите любое целое число");
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+    Console.Write($"{count},");
+    }
+count ++;
+}
