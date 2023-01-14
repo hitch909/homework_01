3a70c4209cb42262618e6770ddeb54d6c9f914b1
hitch909 <evgenij909@yandex.ru>
2023-01-14 14:30:11 +0500

    небольшие правки в task_02

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -1,9 +1,9 @@
 ﻿// максимальное из трех чисел.
 
 Console.WriteLine("введите три числа");
-int A = int.Parse(Console.ReadLine());
-int B = int.Parse(Console.ReadLine());
-int C = int.Parse(Console.ReadLine());
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+int C = int.Parse(Console.ReadLine()!);
 int max = A;
 if (B>A && B>C)
 {
